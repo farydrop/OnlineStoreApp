2c0122abc32ba0b54bca81c1ce5588270154719c
farida <farida.hairullina5@gmail.com>
2024-02-04 02:53:04 +0100

    view in progress

---
+++ app/src/main/java/com/farydrop/onlinestoreapp/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.farydrop.onlinestoreapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.farydrop.onlinestoreapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
